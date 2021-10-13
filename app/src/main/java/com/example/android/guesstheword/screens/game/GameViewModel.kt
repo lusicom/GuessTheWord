f62035dfa3c84f068e158d72d2e04dbf31b4854f
Lucy Holub <lusicomgolub@gmail.com>
2021-10-13 17:18:22 +0300

    1 - added a game-finished event 2 - Added LiveData to the ScoreViewModel 3 - Added Play Again button to navigate to the beginning of the game

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -5,15 +5,19 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 class GameViewModel : ViewModel() {
-    // The current word
+
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
-    // The current score
+
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
     get() = _score
-    // The list of words - the front of the list is the next word to guess
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+    get() = _eventGameFinish
+
     private lateinit var wordList: MutableList<String>
 
     /**
@@ -58,8 +62,9 @@ class GameViewModel : ViewModel() {
      * Moves to the next word in the list
      */
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
-            //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            onGameFinish()
+        } else{
             _word.value = wordList.removeAt(0)
         }
     }
@@ -73,6 +78,14 @@ class GameViewModel : ViewModel() {
         _score.value = (score.value)?.plus(1)
 
         nextWord()
+    }
+
+    fun onGameFinish(){
+        _eventGameFinish.value = true
+    }
+
+    fun onGameFinishCompleate(){
+        _eventGameFinish.value =false
     }
 
     override fun onCleared() {
